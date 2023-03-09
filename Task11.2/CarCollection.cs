8f9e080246952a9a2beb0f2b01d44c25ad38f17f
Dina Nevhod <Dina_Nevhod@epam.com>
2023-03-09 16:52:50 +0100

    Add project files.

---
+++ Task11.2/CarCollection.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task11._2
+{
+    public class CarCollection<T>
+    {
+        public List<string> carCollection = new List<string>();
+        public void AddCar(string name, int year)
+        {
+            carCollection.Add($"{name} - {year}");
+        }
+
+        public string this[int index]
+        {
+            get => carCollection[index];
+        }
+
+        public int NumberOfElements
+        {
+            get { return carCollection.Count; }
+        }
+
+        public void ClearCollection()
+        {
+            carCollection.Clear();
+        }
+    }
+}
---
+++ Task11.2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task11._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CarCollection<int> myList = new CarCollection<int>();
+
+            myList.AddCar("BMW",1990);
+            myList.AddCar("Audi", 1900);
+            myList.AddCar("Lanos", 2013);
+            myList.AddCar("Jeep",2020);
+            myList.AddCar("Opel", 2023);
+            myList.AddCar("Lada", 1963);
+
+            Console.WriteLine(myList[2]);
+            Console.WriteLine(myList.NumberOfElements);
+
+            foreach (var element in myList.carCollection)
+            {
+                Console.Write(element);
+                Console.Write("\n");
+            }
+
+            Console.WriteLine("\nClean");
+            myList.ClearCollection();
+
+            foreach (var element in myList.carCollection)
+            {
+                Console.Write(element);
+                Console.Write(" ");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
